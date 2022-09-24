730ddd49ecc0c5e6432301d346f03cea7e51adf1
Elvira Miasnykova <miasnykovaelvira@gmail.com>
2022-09-24 12:16:52 +0300

    finished components rendering

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import user from "components/Profile/user.json";
 import data from "components/Statistics/data.json";
 import friends from "components/FriendsList/friends.json";
-import transactions from "components/TransactionHistory/transactions.json";
+import transactions from "components/TransactionHistory";
 import { Profile } from "components/Profile/Profile";
 import { Statistics } from "./Statistics/Statistics";
 import { FriendsList } from "./FriendsList/FriendsList";
--- src/components/Statistics/StatisticsList/StatisticsList.js
+++ src/components/Statistics/StatisticsList/StatisticsList.js
@@ -19,3 +19,4 @@ StatisticsList.propTypes = {
         }
     )).isRequired,
 };
+
